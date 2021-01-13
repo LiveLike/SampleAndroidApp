8ceebede96d775fec13eff25683151aed7226a8b
Shivam Verma <shivam@livelikevr.com>
2021-01-13 13:32:44 +0530

    add custom cheer meter

--- app/src/main/java/com/livelike/engagementsdksample/widget/WidgetActivity.kt
+++ app/src/main/java/com/livelike/engagementsdksample/widget/WidgetActivity.kt
@@ -44,7 +44,9 @@ class WidgetActivity : AppCompatActivity() {
             }
 
             override fun createCheerMeterView(cheerMeterWidgetModel: CheerMeterWidgetmodel): View? {
-                return null
+                return CustomCheerMeter(this@WidgetActivity).apply {
+                    this.cheerMeterWidgetmodel = cheerMeterWidgetModel
+                }
             }
 
             override fun createImageSliderWidgetView(imageSliderWidgetModel: ImageSliderWidgetModel): View? {
